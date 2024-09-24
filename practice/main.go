ca6b8320307d4b4851c6aa58a2d060dc3a0de3c5
Lucas <ledinhluan1997@gmail.com>
2024-09-23 17:33:45 -0700

    0304

--- practice/main.go
+++ practice/main.go
@@ -2,9 +2,29 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 func main() {
-	var colors = [3]string{"Red", "Green", "Blue"}
+	var colors = []string{"Red", "Green", "Blue"}
 	fmt.Println(colors)
+	colors = append(colors, "Purple")
+	fmt.Println(colors)
+
+	colors = append(colors[:len(colors)-1])
+	fmt.Println(colors)
+
+	number := make([]int, 5)
+	number[0] = 10
+	number[1] = 7
+	number[2] = 15
+	number[3] = 1
+	number[4] = 4
+	fmt.Println(number)
+
+	number = append(number, 19)
+	fmt.Println(number)
+
+	sort.Ints(number)
+	fmt.Println(number)
 }
