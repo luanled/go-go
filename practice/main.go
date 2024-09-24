8ede47dd989c1fbdcec4aa4c8278d1fb934e93dc
Lucas <ledinhluan1997@gmail.com>
2024-09-23 22:10:16 -0700

    ex

--- practice/main.go
+++ practice/main.go
@@ -5,14 +5,43 @@ import (
 )
 
 func main() {
-	poodle := Dog{"Poodle", 10}
-	fmt.Println(poodle)
-	fmt.Printf("%+v\n", poodle)
-	fmt.Printf("Breed: %v\nWeight: %v\n", poodle.Breed, poodle.Weight)
+	// poodle := Dog{"Poodle", 10, "Woof!"}
+	// fmt.Println(poodle)
+	// fmt.Printf("%+v\n", poodle)
+	// fmt.Printf("Breed: %v\nWeight: %v\n", poodle.Breed, poodle.Weight)
+	// poodle.Speak()
+	// poodle.SpeakLikeCrazy()
+	fmt.Println(addValues(1.55, 3.65))
 }
 
 // Dog is a struct
 type Dog struct {
 	Breed  string
 	Weight int
+	Sound  string
+}
+
+func (d Dog) Speak() {
+	fmt.Println(d.Sound)
+}
+
+func (d Dog) SpeakLikeCrazy() {
+	d.Sound = fmt.Sprintf("%v %v %v %v %v %v %v %v %v %v", d.Sound, d.Sound, d.Sound, d.Sound, d.Sound, d.Sound, d.Sound, d.Sound, d.Sound, d.Sound)
+	fmt.Println(d.Sound)
+}
+
+func addValues(value1, value2 float64) float64 {
+	return value1 + value2
+}
+
+func subtractValues(value1, value2 float64) float64 {
+	return value1 - value2
+}
+
+func multiplyValues(value1, value2 float64) float64 {
+	return value1 * value2
+}
+
+func divideValues(value1, value2 float64) float64 {
+	return value1 / value2
 }
