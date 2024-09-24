017d6a17bcbd9f093b498d3faa84fbcf380e77fc
Lucas <ledinhluan1997@gmail.com>
2024-09-23 22:30:16 -0700

    0601

--- calculator/main.go
+++ calculator/main.go
@@ -2,8 +2,28 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
 )
 
 func main() {
-	fmt.Println("Files")
+	content := "hello from go!"
+	file, err := os.Create("./fromString.txt")
+	checkErr(err)
+	length, err := io.WriteString(file, content)
+	checkErr(err)
+	fmt.Printf("Wrote a file with %v characters\n", length)
+	defer file.Close()
+	defer readFile("./fromString.txt")
+}
+func readFile(name string) {
+	data, err := ioutil.ReadFile(name)
+	checkErr(err)
+	fmt.Println("Text from file: ", string(data))
+}
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
