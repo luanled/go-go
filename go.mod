ed5bc5a9af3fcf9126d32079538b1bfceb8c2c06
Lucas <ledinhluan1997@gmail.com>
2024-09-23 18:41:48 -0700

    0401

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/luanled/go-go
+
+go 1.23.1
--- practice/main.go
+++ practice/main.go
@@ -5,5 +5,17 @@ import (
 )
 
 func main() {
-	fmt.Println("Conditional logic")
+	fmt.Println(checkNum(0))
+}
+func checkNum(a int) string {
+	var result string
+
+	if a < 0 {
+		result = "Less than 0"
+	} else if a == 0 {
+		result = "Is Zero"
+	} else {
+		result = "Greater than 0"
+	}
+	return result
 }
