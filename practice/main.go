eff6e248edeadb130e9b3927076b23d81849b2c3
Lucas <ledinhluan1997@gmail.com>
2024-09-23 19:13:37 -0700

    0403

--- practice/main.go
+++ practice/main.go
@@ -6,5 +6,34 @@ import (
 
 func main() {
 	colors := []string{"Red", "Green", "Blue"}
-	fmt.Println(colors)
+	// fmt.Println(colors)
+
+	// for i := 0; i < len(colors); i++ {
+	// 	fmt.Println(colors[i])
+	// }
+
+	// for i := range colors {
+	// 	fmt.Println(colors[i])
+	// }
+
+	for _, color := range colors {
+		fmt.Println(color)
+	}
+
+	value := 1
+	for value < 10 {
+		fmt.Println("Value:", value)
+		value++
+	}
+
+	sum := 1
+	for {
+		sum += sum
+		fmt.Println("Sum:", sum)
+
+		if sum > 10000 {
+			break
+		}
+
+	}
 }
