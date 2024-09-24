6a3200746eee679d5140e4a0dac3ca623ebfcb1a
Lucas <ledinhluan1997@gmail.com>
2024-09-23 19:01:55 -0700

    0402

--- practice/main.go
+++ practice/main.go
@@ -3,11 +3,29 @@ package main
 import (
 	"fmt"
 	"math/rand"
-	"time"
 )
 
 func main() {
-	rand.Seed(time.Now().Unix())
+
 	dow := rand.Intn(7) + 1
 	fmt.Println("Day", dow)
+
+	var res string
+	switch dow {
+	case 1:
+		res = "Sunday the king plays"
+	case 2:
+		res = "It's Monday!"
+	case 3:
+		res = "It's Tuesday!"
+	case 4:
+		res = "It's Wednesday!"
+	case 5:
+		res = "It's Thursday!"
+	case 6:
+		res = "It's Friday!"
+	case 7:
+		res = "It's Saturday!"
+	}
+	fmt.Println(res)
 }
