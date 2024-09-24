51e4c741fcc8f4b5ad325d42f2e6b40f4bca7999
Lucas <ledinhluan1997@gmail.com>
2024-09-23 17:14:00 -0700

    03_03push

--- practice/main.go
+++ practice/main.go
@@ -6,4 +6,13 @@ import (
 
 func main() {
 	fmt.Println("Arrays")
+
+	var colors [3]string
+	colors[0] = "Red"
+	colors[1] = "Green"
+	colors[2] = "Blue"
+	fmt.Println(colors)
+
+	var numbers = [5]int{1, 2, 3, 4, 5}
+	fmt.Println(numbers)
 }
