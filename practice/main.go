b3f15c812519fbc304c4c692cae8f6455c23154c
Lucas <ledinhluan1997@gmail.com>
2024-09-23 17:52:38 -0700

    0305

--- practice/main.go
+++ practice/main.go
@@ -2,8 +2,40 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 func main() {
-	fmt.Println("Maps")
+	states := make(map[string]string)
+	fmt.Println(states)
+
+	states["CA"] = "California"
+	states["OR"] = "Oregon"
+	states["TX"] = "Texas"
+	states["OH"] = "Ohio"
+	states["FL"] = "Florida"
+	states["AR"] = "Arizona"
+
+	fmt.Println(states)
+
+	// california := states["CA"]
+	// fmt.Println(california)
+
+	fmt.Println(states)
+
+	for k, v := range states {
+		fmt.Printf("%v: %v\n", k, v)
+	}
+	keys := make([]string, len(states))
+	i := 0
+	for k := range states {
+		keys[i] = k
+		i++
+	}
+	sort.Strings(keys)
+	fmt.Println(keys)
+
+	for i := range keys {
+		fmt.Println(states[keys[i]])
+	}
 }
