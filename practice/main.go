5c99b0c0e0b8fc99779ec05aa60a128095d328a1
Lucas <ledinhluan1997@gmail.com>
2024-09-23 21:17:16 -0700

    0501

--- practice/main.go
+++ practice/main.go
@@ -5,5 +5,26 @@ import (
 )
 
 func main() {
-	fmt.Println("Functions")
+	doSomething()
+	multiply := mulValues(99, 10)
+	fmt.Println(multiply)
+
+	mulmoreandmore, lengthofmul := mulMore(1, 2, 3, 4, 5)
+	fmt.Printf("Mul mote = %v and it contains %v values", mulmoreandmore, lengthofmul)
+}
+
+func doSomething() {
+	fmt.Println("Doing sth")
+}
+
+func mulValues(val1, val2 int) int {
+	return val1 * val2
+}
+
+func mulMore(vals ...int) (int, int) {
+	total := 1
+	for _, v := range vals {
+		total *= v
+	}
+	return total, len(vals)
 }
